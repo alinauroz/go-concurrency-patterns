9f68393ea58300645d122472612ebf7a43fddf98
nauroz <alinauroze@hotmail.com>
2023-03-06 23:06:24 +0500

    default case explanation

--- loop/basic.go
+++ loop/basic.go
@@ -1,5 +1,16 @@
 /*
-	A basic for select loop
+	DEFAULT case in for select loop
+
+	In this example, a channel is created which will be closed after 5seconds. In PART I,
+	a goroutine sleeps for 5 seconds and then closes the channel.
+
+	In PART II, there is a for select loop which will run infinitely until there is something
+	to be read on channel. This "for select" is going to take 5 seconds (simulated time.Sleep)
+	which means it will stop the execution of code for 5 seconds. This 5 seconds of CPU time will
+	be wasted.
+
+	This problem can be solved by default case. When none of the conditions for the select statement
+	is fulfilled, default case is executed.
 */
 
 package loop
