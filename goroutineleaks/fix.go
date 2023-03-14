c73e7d06ab953a6caa6d14dbc69135b81ee59fee
nauroz <alinauroze@hotmail.com>
2023-03-15 01:27:55 +0500

    explanation

--- goroutineleaks/fix.go
+++ goroutineleaks/fix.go
@@ -1,3 +1,17 @@
+/*
+	Fixing Goroutine Leak Using a Channel
+
+	In Demo function it can be seen how does a goroutine leak look. The same function
+	is used in this example but it has been changed a bit. A done channel is passed to
+	the doWork function.
+
+	The program has been divided into 3 parts. In first part, doWork is defined and
+	called. In Part II, there is a goroutine which closes the done channel after 1 second.
+	So it does not matter how long the process will take to terminate, the goroutine will
+	return after after second when the done channel is closed by the goroutine in PART II.
+
+*/
+
 package goroutineleaks
 
 import (
@@ -7,7 +21,8 @@ import (
 
 func FixByChannel() {
 
-	doWork := func(strings <-chan string, done <-chan interface{}) <-chan interface{} {
+	//PART I
+	doWork := func(done <-chan interface{}, strings <-chan string) <-chan interface{} {
 		completed := make(chan interface{})
 
 		go func() {
@@ -26,13 +41,15 @@ func FixByChannel() {
 	}
 
 	done := make(chan interface{})
-	completed := doWork(nil, done)
+	completed := doWork(done, nil)
 
+	// PART II
 	go func() {
 		defer close(done)
 		time.Sleep(1 * time.Second)
 	}()
 
+	// PART II
 	<-completed
 	fmt.Println("DONE")
 
