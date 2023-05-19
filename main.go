7574202c9f76677cac97ecfef71e7d7c5563ff75
nauroz <alinauroze@hotmail.com>
2023-05-20 01:01:51 +0500

    pipeline with generators

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	pipeline.Idea()
+	pipeline.PipelineWithGenerator()
 }
--- pipeline/basic.go
+++ pipeline/basic.go
@@ -33,3 +33,63 @@ func Idea() {
 		),
 	)
 }
+
+// uses generator for memory optimizations
+func PipelineWithGenerator() {
+	generator := func(done <-chan interface{}, integers ...int) <-chan int {
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for _, i := range integers {
+				select {
+				case <-done:
+					return
+				case intStream <- i:
+				}
+			}
+		}()
+		return intStream
+	}
+
+	add := func(done <-chan interface{}, stream <-chan int, number int) <-chan int {
+		addedStream := make(chan int)
+
+		go func() {
+			defer close(addedStream)
+			for i := range stream {
+				select {
+				case <-done:
+					return
+				case addedStream <- (i + number):
+				}
+			}
+		}()
+
+		return addedStream
+	}
+
+	multiply := func(done <-chan interface{}, stream <-chan int, multiplier int) <-chan int {
+		multipliedStream := make(chan int)
+		go func() {
+			defer close(multipliedStream)
+			for i := range stream {
+				select {
+				case <-done:
+					return
+				case multipliedStream <- (i * multiplier):
+				}
+			}
+		}()
+		return multipliedStream
+	}
+
+	arr := []int{1, 2, 3}
+	done := make(chan interface{})
+	inputStream := generator(done, arr...)
+	resultStream := multiply(done, add(done, multiply(done, inputStream, 2), 1), 10)
+	for i := range resultStream {
+		fmt.Println(i)
+	}
+}
+
+//keywords: generators, pipelines, stages
