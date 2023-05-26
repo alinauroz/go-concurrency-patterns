9bdb7f9b66d2930cf0b349b0f8a680d284f12d58
nauroz <alinauroze@hotmail.com>
2023-05-27 01:32:11 +0500

    limited repeat

--- generator/Generators.go
+++ generator/Generators.go
@@ -39,5 +39,39 @@ func RepeatDemo() {
 	}
 
 	fmt.Println("Generator stopped!")
+}
 
+func LimitedRepeat(done <-chan interface{}, stream <-chan int, limit int) <-chan interface{} {
+	limitedStream := make(chan interface{})
+
+	go func() {
+		defer close(limitedStream)
+		for i := 0; i < limit; i++ {
+			select {
+			case <-done:
+				return
+			case limitedStream <- <-stream:
+			}
+		}
+	}()
+
+	return limitedStream
+}
+
+func LimitedRepeatDemo() {
+	data := []int{1, 2, 3, 4, 5}
+	stop := make(chan interface{})
+	stream := Repeat(stop, data)
+	limitedStream := LimitedRepeat(stop, stream, 10)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		close(stop)
+	}()
+
+	for i := range limitedStream {
+		fmt.Println(i)
+	}
+
+	fmt.Println("Generator stopped!")
 }
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	generator.RepeatDemo()
+	generator.LimitedRepeatDemo()
 }
